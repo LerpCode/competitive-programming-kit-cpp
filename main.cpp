0d842c9bd22f06c40d9d17e834b2a840785004ae
Lerp <91041708+LerpCode@users.noreply.github.com>
2023-07-06 20:17:46 +0700

    6/7/2023

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx2,popcnt,lzcnt,abm,bmi,bmi2,fma,tune=native")
+#include <bits/stdc++.h>
+#define ull unsigned long long
+#define ll long long
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+cout.tie(NULL);
+
+
+
+
+return 0;
+}
