3b91b3098a2627fd3a736567077b39b83a931a7f
Lerp <91041708+LerpCode@users.noreply.github.com>
2023-07-29 19:04:47 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
+#include <bits/stdc++.h>
 #pragma GCC optimize("Ofast,unroll-loops")
 #pragma GCC target("avx2,popcnt,lzcnt,abm,bmi,bmi2,fma,tune=native")
-#include <bits/stdc++.h>
+#define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
 #define ull unsigned long long
 #define ll long long
 #define MOD 1000000007
