5845000a3ec3ada341cc4f36a5049ae9065a2843
Lerp <91041708+LerpCode@users.noreply.github.com>
2023-08-23 11:55:12 +0700

    Update main.cpp, quicker naive code, define menu

--- main.cpp
+++ main.cpp
@@ -1,19 +1,48 @@
+#pragma GCC optimize("Ofast")  
+#pragma GCC optimize("unroll-loops")  
+#pragma GCC target("avx,avx2,tune=native")  
+#pragma GCC optimize("inline")  
+#pragma GCC optimize("fast-math")  
+#pragma GCC optimize("Os")  
+#pragma GCC optimize("O0")
+
 #include <bits/stdc++.h>
-#pragma GCC optimize("Ofast,unroll-loops")
-#pragma GCC target("avx2,popcnt,lzcnt,abm,bmi,bmi2,fma,tune=native")
-#define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
-#define ull unsigned long long
-#define ll long long
-#define MOD 1000000007
 using namespace std;
 
+
+#define fast() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define ull unsigned long long int
+#define ll long long
+#define MOD 1000000007
+#define maxN 100007
+#define PI acos(-1)
+#define space char(32)
+#define el '\n'
+#define str(s) getline(cin,s)
+#define ign() cin.ignore()
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) ((a * b)/__gcd(a,b))
+#define rev(s) reverse(s.begin(),s.end())
+#define maxarr(a,n) *max_element(a, a + n)
+#define minarr(a,n) *min_element(a, a + n)
+#define one(a,n) int a[n]
+#define oll(a,n) ll a[n]
+#define two(a,m,n) int a[m][n]
+#define tll(a,m,n) ll a[m][n]
+#define set(a,n) memset(a,n,sizeof(a))
+#define lower(s) for(auto& x : s) {x = tolower(x);}
+#define upper(s) for(auto& x : s) {x = toupper(x);}
+#define isnum(c) (c >= '0' && c <= '9')
+#define isup(c) (c >= 'A' && c <= 'Z')
+#define islow(c) (c >= 'a' && c <= 'z')
+#define isletter(c) ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+
+void solve(){
+   cout << 10 * cos(PI/6);
+}
+
 int main(){
-ios_base::sync_with_stdio(false);
-cin.tie(NULL);
-cout.tie(NULL);
-
-
-
-
-return 0;
+    fast();
+    solve();
+    return 0;
 }
