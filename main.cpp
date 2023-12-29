2b241fb3bdfc9204518cdb7c0f1b52040bdde6d5
Lerp <91041708+LerpCode@users.noreply.github.com>
2023-12-29 18:42:58 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,36 +1,43 @@
-#pragma GCC optimize("Ofast")  
-#pragma GCC optimize("unroll-loops")  
-#pragma GCC target("avx,avx2,tune=native")  
-#pragma GCC optimize("inline")  
-#pragma GCC optimize("fast-math") 
-
 #include <bits/stdc++.h>
 using namespace std;
 
+#pragma GCC optimize("Ofast,unroll-loops") 
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimize("inline")  
+#pragma GCC optimize("fast-math")
+
+using str = string;
+using d   = double;
+using ld  = long double;
+using ll  = long long;
+using ull = unsigned long long;
+
+const int maxN = 1e6 + 5;
+const ll  MOD  = 1e9 + 7;
+const ull inf  = 1e18 + 10;
+
+
+template<typename T> T gcd(T a, T b){return(b?__gcd(a,b):a);} 
+template<typename T> T lcm(T a, T b){return(a*(b/gcd(a,b)));}
+template<typename T> T fmax(T a,T b){return (a > b) ? a : b;}
+template<typename T> T fmin(T a,T b){return (a < b) ? a : b;}
+
 
 #define fast() ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-#define ull unsigned long long int
-#define ll long long
-#define MOD 1000000007
-#define maxN 100007
 #define PI acos(-1)
 #define space char(32)
 #define el '\n'
 #define get(s) getline(cin,s)
 #define ign() cin.ignore()
-#define gcd(a,b) __gcd(a,b)
-#define lcm(a,b) ((a * b)/__gcd(a,b))
 #define rev(s) reverse(s.begin(),s.end())
 #define maxarr(a,n) *max_element(a, a + n)
 #define minarr(a,n) *min_element(a, a + n)
 #define one(a,n) int a[n]
-#define odu(a,n) double a[n]
+#define odu(a,n) d a[n]
 #define oll(a,n) ll a[n]
 #define two(a,m,n) int a[m][n]
 #define tll(a,m,n) ll a[m][n]
 #define set(a,n) memset(a,n,sizeof(a))
-#define lower(s) for(auto& x : s) {x = tolower(x);}
-#define upper(s) for(auto& x : s) {x = toupper(x);}
 #define isnum(c) (c >= '0' && c <= '9')
 #define isup(c) (c >= 'A' && c <= 'Z')
 #define islow(c) (c >= 'a' && c <= 'z')
@@ -39,11 +46,18 @@ using namespace std;
 #define FORJ(a,b,x) for(int j = a;j < b;j += x)
 #define FORIE(a,b,x) for(int i = a;i <= b;i += x)
 #define FORID(a,b,x) for(double i = a;i < b;i += x)
+#define vi(a,n) vector<int> a(n)
+#define vb(a,b) vector<bool> a(b)
+
+
+
+
 void solve(){
- 
+
 }
+
 int main(){
     fast();
-    solve();
-    return 0;
+    return solve(),0;
+
 }
