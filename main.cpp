b09fc82259ac74ef29cc648babebfa12ec6e38fc
Lerp <91041708+LerpCode@users.noreply.github.com>
2024-01-28 00:06:31 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,9 +2,8 @@
 using namespace std;
 
 #pragma GCC optimize("Ofast,unroll-loops") 
-#pragma GCC target("avx,avx2,fma")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
 #pragma GCC optimize("inline")  
-#pragma GCC optimize("fast-math")
 
 using str = string;
 using d   = double;
